708614caf947e192666812ebdbb3a08555f1c87e
Collin Graves <collinrgraves@gmail.com>
2015-06-13 16:48:24 -0500

    doctored front end and installed rew relic

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homes#index'
+
   match '/destroy_session', to: 'sessions#destroy', via: :get, method: :delete
   get '/auth/:provider/callback', to: 'sessions#create'
 
-  resources :rants, :users, :sessions
+  resources :rants, only: [:new, :create]
+  resources :sessions, only: [:create, :destroy]
 end
