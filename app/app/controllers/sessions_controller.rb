00bcd6bc00deea6f5aea00dcb4bab519f35a393a
Collin Graves <collinrgraves@gmail.com>
2015-06-13 18:24:20 -0500

    explicit respond_to block for sessions#create

--- app/app/controllers/sessions_controller.rb
+++ app/app/controllers/sessions_controller.rb
@@ -2,7 +2,16 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
-    redirect_to new_rant_path
+
+    respond_to do |format|
+      format.html
+        if @user.valid?
+          redirect_to new_rant_path
+          return
+        end
+      redirect_to root_path
+    end
+
   end
 
   def destroy
