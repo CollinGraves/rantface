db7c4d5fa2dca1feeec2e1560c6c3386fb19ef6b
Collin Graves <collinrgraves@gmail.com>
2015-06-12 19:49:32 -0500

    handle logout and no current_user on rant creation

--- app/app/controllers/rants_controller.rb
+++ app/app/controllers/rants_controller.rb
@@ -1,6 +1,8 @@
 class RantsController < ApplicationController
   before_action :set_rant, only: [:show, :edit, :update, :destroy]
 
+  rescue_from Exception, with: :rant_rescue
+
   def index
     @rants = Rant.all
   end
@@ -15,17 +17,19 @@ class RantsController < ApplicationController
   def create
     begin
       @rant = Rant.new(rant_params)
+
+      if current_user.nil?
+        flash[:errors] = "You need to log in first"
+        render action: :new
+        return
+      end
+
       @rant.user_id = current_user.id
       @rant.save
 
       respond_to do |format|
         format.html { redirect_to root_path }
       end
-
-      rescue Twitter::Error => e
-        flash[:errors] = e.message
-        redirect_to new_rant_path
-      return
     end
   end
 
@@ -37,6 +41,11 @@ class RantsController < ApplicationController
 
   def rant_params
     params.require(:rant).permit(:user_id, :body)
+  end
+
+  def rant_rescue(e = nil)
+    flash[:errors] = e.message if e
+    render action: :new
   end
 
 end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#index'
+  match '/destroy_session', to: 'sessions#destroy', via: :get, method: :delete
   get '/auth/:provider/callback', to: 'sessions#create'
 
   resources :rants, :users, :sessions
