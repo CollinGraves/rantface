9323c04098d3ebb4b06236adb308a84deba3bbcf
Collin Graves <collinrgraves@gmail.com>
2015-06-12 19:05:08 -0500

    update omniauth init

--- app/config/initializers/omniauth.rb
+++ app/config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
+  unless Rails.env.development? || Rails.env.test?
+    provider :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"]
+  else
+    provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
+  end
 end
